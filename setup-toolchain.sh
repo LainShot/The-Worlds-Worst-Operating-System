2650a88aeadcd0b79294b527aa7fca13ce0e23c8
LainShot <19312376+LainShot@users.noreply.github.com>
2021-05-31 17:40:18 +0100

    added all the headers needed for types.h file.

---
+++ setup-toolchain.sh
@@ -0,0 +1,33 @@
+if [ "$EUID" -ne 0 ]
+  then 
+  echo " __    _____ _     _____ _       _____ "
+  echo "|  |  |  _  |_|___|   __| |_ ___|_   _|"
+  echo "|  |__|     | |   |__   |   | . | | |  "
+  echo "|_____|__|__|_|_|_|_____|_|_|___| |_|  "
+  echo "                                       "
+  echo "======================================="
+  echo "You need to run this as root or sudo."
+  exit
+fi
+echo " __    _____ _     _____ _       _____ "
+echo "|  |  |  _  |_|___|   __| |_ ___|_   _|"
+echo "|  |__|     | |   |__   |   | . | | |  "
+echo "|_____|__|__|_|_|_|_____|_|_|___| |_|  "
+echo "                                       "
+echo "======================================="
+echo "TOOL CHAIN SETUP SCRIPT"
+echo "======================================="
+echo "Installing software needed"
+apt-get install -y nasm
+apt-get install -y xorriso
+apt-get install -y grub-pc-bin
+apt-get install -y grub-common
+echo "All software installed"
+echo "I am now going setup the toolchain."
+wget https://github.com/lordmilko/i686-elf-tools/releases/download/7.1.0/i686-elf-tools-linux.zip
+echo "creating toolchain folder "
+mkdir toolchain
+echo "unziping toolchain into folder"
+unzip i686-elf-tools-linux -d ./toolchain
+echo "All installed you can now run make build-x86_64"
+
---
+++ src/intf/types.h
@@ -0,0 +1,7 @@
+/*
+ * int-ll64 is used everywhere now.
+ */
+#include <int-ll64.h>
+
+#endif /* _ASM_GENERIC_TYPES_H */
+
